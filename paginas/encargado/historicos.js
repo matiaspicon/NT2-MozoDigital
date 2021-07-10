8cd12c2e7592887205033ccf11e02369e5ba8afe
matiaspicon <matiasgpicon@gmail.com>
2021-07-10 15:53:01 -0300

    delete searchbar in historicos view

--- paginas/encargado/historicos.js
+++ paginas/encargado/historicos.js
@@ -9,19 +9,12 @@ import GlobalContext from "../../components/global/context";
 export default function Index({ navigation }) {
 
   const context = useContext(GlobalContext);
-   const [filtro, setFiltro] = useState("")
   function filtroRol(pedido) {
     return pedido.restaurante == context.restaurante.idRestaurante && pedido.sucursal == context.restaurante.idSucursal && pedido.estado == "Entregado"
   }
 
   return (
     <SafeAreaView style={styles.container}>
-        <SearchBar
-          placeholder="Buscar"
-          onChangeText={(text) => setFiltro(text)}
-          value={filtro}
-          round="true"
-        />
         <ScrollView>
           <Pedidos navigation={navigation} filtroRol={filtroRol} />
         </ScrollView>
